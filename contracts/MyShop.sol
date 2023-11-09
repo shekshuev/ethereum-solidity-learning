7512c11c9cd78f0437622017e1868eb6a3bc7d12
Sergei Shekshuev <sergei.shekshuev@gmail.com>
2023-11-09 09:46:09 +0300

    first contract

---
+++ contracts/MyShop.sol
@@ -0,0 +1,23 @@
+// SPDX-Licence-Identifier: MIT
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MyShop {
+    address public owner;
+    mapping (address => uint) public payments;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function payForItem() public payable {
+        payments[msg.sender] = msg.value;
+    }
+
+    function withdrawAll() public {
+        address payable  _to = payable (owner);
+        address _thisContract = address(this);
+        _to.transfer(_thisContract.balance);
+    }
+}
